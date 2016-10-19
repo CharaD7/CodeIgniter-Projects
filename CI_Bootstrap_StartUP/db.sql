ee088cfda7ed133dacbd8e73c8c4dd033929214d
Kiril Kirkov <kiro@inv.bg>
2016-10-19 12:25:46 +0300

    add new database structure

--- CI_Bootstrap_StartUP/db.sql
+++ CI_Bootstrap_StartUP/db.sql
@@ -1,27 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 19, 2016 at 12:19 PM
+-- Server version: 5.5.52-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.19
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `articles`
+--
+
 CREATE TABLE IF NOT EXISTS `articles` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `basic_description` varchar(500) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `thumb` varchar(255) NOT NULL,
+  `article_id` int(10) unsigned NOT NULL,
+  `folder` int(10) unsigned DEFAULT NULL COMMENT 'folder with images',
   `image` varchar(255) NOT NULL,
-  `description` longtext NOT NULL,
-  `url` varchar(255) NOT NULL,
-  `category` varchar(20) NOT NULL,
   `time` int(10) unsigned NOT NULL COMMENT 'time created',
   `time_update` int(10) unsigned NOT NULL COMMENT 'time updated',
   `visibility` tinyint(1) NOT NULL DEFAULT '1',
-  `language` varchar(5) NOT NULL,
+  `category` varchar(20) NOT NULL,
+  `in_slider` tinyint(1) NOT NULL DEFAULT '0',
+  `url` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `articles`
+--
+
+INSERT INTO `articles` (`id`, `article_id`, `folder`, `image`, `time`, `time_update`, `visibility`, `category`, `in_slider`, `url`) VALUES
+(1, 1, 1476867333, '13880406_1325840574112545_4476736967360037629_n7.jpg', 1476867345, 1476868375, 1, 'test', 1, 'b_1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
 
 CREATE TABLE IF NOT EXISTS `categories` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(2, 'test');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history`
+--
 
 CREATE TABLE IF NOT EXISTS `history` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -31,31 +80,78 @@ CREATE TABLE IF NOT EXISTS `history` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `languages`
+--
+
 CREATE TABLE IF NOT EXISTS `languages` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `abbr` varchar(5) NOT NULL,
   `name` varchar(30) NOT NULL,
+  `currency` varchar(10) CHARACTER SET utf8 NOT NULL,
+  `flag` varchar(255) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
-CREATE TABLE IF NOT EXISTS `plugins` (
+--
+-- Dumping data for table `languages`
+--
+
+INSERT INTO `languages` (`id`, `abbr`, `name`, `currency`, `flag`) VALUES
+(1, 'bg', 'bulgarian', 'лв', 'bg.jpg'),
+(2, 'en', 'english', '$', 'en.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `translations`
+--
+
+CREATE TABLE IF NOT EXISTS `translations` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `enabled` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `title` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `description` longtext CHARACTER SET utf8 NOT NULL,
+  `basic_description` text CHARACTER SET utf8 NOT NULL,
+  `abbr` varchar(5) CHARACTER SET utf8 NOT NULL,
+  `for_id` int(11) NOT NULL COMMENT ' ',
+  `type` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `art_id_abbr` (`abbr`,`for_id`,`type`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `translations`
+--
+
+INSERT INTO `translations` (`id`, `title`, `description`, `basic_description`, `abbr`, `for_id`, `type`, `name`) VALUES
+(1, 'b1', '<p>b1</p>\r\n', '<p>b</p>\r\n', 'bg', 1, 'article', ''),
+(2, 'e1', '<p>e1</p>\r\n', '<p>e</p>\r\n', 'en', 1, 'article', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `password` varchar(32) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `notify` tinyint(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
--- default password is admin
+-- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@admin.admin');
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `notify`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@admin.admin', 0);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
