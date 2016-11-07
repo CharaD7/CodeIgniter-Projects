122a01a27d229bf0496a0c396ede14e504ee6509
Kiril Kirkov <kiro@inv.bg>
2016-11-07 10:33:05 +0200

    Remove unnecessary site

--- podobrisebesi.bg/README.md
+++
@@ -1,3 +0,0 @@
-# Подобри себе си
-
-![alt tag](https://raw.githubusercontent.com/kirilkirkov/CodeIgniter-Projects/master/podobrisebesi.bg/screenshot.png)
--- podobrisebesi.bg/application/views/view_article.php
+++
@@ -1,6 +0,0 @@
-<div id="view_article">
-    <div class="container">
-        <h1><?= $article['title'] ?></h1>
-        <div><?= $article['description'] ?></div>
-    </div>
-</div>
